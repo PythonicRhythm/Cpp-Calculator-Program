7346bee64c6349e806d10d1924cf727a6ea39827
Anthony Alvarez <101856161+PythonicRhythm@users.noreply.github.com>
2024-02-20 08:56:28 -0600

    no catch testing yet but string inputs work now!

--- code/calculator.cpp
+++ code/calculator.cpp
@@ -12,6 +12,7 @@
 
 
 #include <iostream>
+#include <sstream>
 #include <stdexcept>
 #include <string>
 
@@ -20,6 +21,7 @@
 char const number = '8';    // a floating-point number
 char const quit = 'q';      // an exit command
 char const print = ';';     // a print command
+char const nullTerm = '\0'; // null term for strings
 
 class token
 {
@@ -59,6 +61,8 @@ class token_stream
     bool full;       // is there a token in the buffer?
     token buffer;    // here is where we keep a Token put back using
                      // putback()
+    std::istream* source = nullptr;
+
 public:
     // user interface:
     token get();            // get a token
@@ -69,7 +73,26 @@ public:
     token_stream()
       : full(false)
       , buffer('\0')
+      , source(&std::cin)
     {
+    }
+
+    token_stream(std::string const& str)
+      : full(false)
+      , buffer('\0')
+    {
+        set_source(str);
+    }
+
+    void set_source(std::string const& str)
+    {
+        source = new std::istringstream (str);
+;
+    }
+
+    bool has_more_tokens()
+    {
+        return source->good();
     }
 };
 
@@ -95,7 +118,8 @@ token token_stream::get()    // read a token from the token_stream
 
     // note that >> skips whitespace (space, newline, tab, etc.)
     char ch;
-    std::cin >> ch;
+    //std::cin >> ch;
+    *source >> ch;
 
     switch (ch)
     {
@@ -107,6 +131,7 @@ token token_stream::get()    // read a token from the token_stream
     case '-':
     case '*':
     case '/':
+    case '\0':
         return token(ch);    // let each character represent itself
     case '.':
     case '0':
@@ -120,9 +145,11 @@ token token_stream::get()    // read a token from the token_stream
     case '8':
     case '9':
     {
-        std::cin.putback(ch);    // put digit back into the input stream
+        //std::cin.putback(ch);    // put digit back into the input stream
+        source->putback(ch);
         double val;
-        std::cin >> val;    // read a floating-point number
+        //std::cin >> val;    // read a floating-point number
+        *source >> val;
         return token(val);
     }
     default:
@@ -143,7 +170,8 @@ void token_stream::ignore(char c)
 
     // now search input:
     char ch = 0;
-    while (std::cin >> ch)
+    //while (std::cin >> ch)
+    while(*source >> ch)
     {
         if (ch == c)
             break;
@@ -228,9 +256,10 @@ void clean_up_mess()
     ts.ignore(print);
 }
 
-void calculate()
+void calculate(std::string const& commands = "")
 {
-    while (std::cin)
+    if(!commands.empty()) ts = token_stream(commands);
+    while (ts.has_more_tokens())
     {
         try
         {
@@ -243,7 +272,12 @@ void calculate()
 
             if (t.kind() == quit)
                 return;    // ‘q’ for “quit”
-
+            
+            if (t.kind() == nullTerm)
+            {
+                std::cout << "Command String read..." << std::endl;
+                continue;
+            }
             ts.putback(t);
 
             std::cout << result << expression() << std::endl;
@@ -260,7 +294,11 @@ int main()
 {
     try
     {
-        calculate();
+        calculate("3+5; 24+2; 1*50; 3/3; (23*2)+(23/2);");
+        calculate("(1+2)*(3-2)/(6-3); ((33+3)-(3/2)*25); (23+45)-25*(6000-23); ((23+45)-25)*(6000-23); ((23+45)-25)*((6000-23)-32*100);");
+        calculate("(((34*3)))-(25-25)*10; 34*3*3*2; 3/3/3/3; 25; 32-2000;");
+        //calculate();
+        //calculate();
         return 0;
     }
     catch (...)
